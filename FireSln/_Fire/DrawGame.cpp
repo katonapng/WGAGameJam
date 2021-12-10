153025d8d438f075ef2cbf0562a127f1f08190db
igorkkkk <igoranchikkuzmin@yandex.ru>
2021-12-11 02:37:02 +0300

    few small corrections
    So, I included function to checking the damage of our player. Also You can delete all useless files, if decided that they are extra for our task

---
+++ FireSln/_Fire/DrawGame.cpp
@@ -0,0 +1,1 @@
+#include "DrawGame.h"
--- FireSln/_Fire/main.cpp
+++ FireSln/_Fire/main.cpp
@@ -3,8 +3,10 @@
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Event.hpp>
 #include <string.h>
+#include <algorithm>
 
 
+auto differernce_lenght(std::string a, std::string b)->int;
 //using namespace sf;
 //using namespace std;
 int main()
@@ -14,6 +16,8 @@ int main()
                                           //2 is object position
   std::string input_answer;
   std::string output_text;
+  float health_damage 0;
+  //TODO(igorkkkk) add text downloading to output_text variable
 
   sf::RenderWindow window(sf::VideoMode(640, 480), "Game"); //TODO(igorkkkk) change game name
 
@@ -25,12 +29,18 @@ int main()
       if (event.type == sf::Event::Closed)
         window.close();
 
-      //input answer string
-      if (event.type == sf::Event::KeyPressed)
-      {
-        input_answer += event.key.code;
+      //input answer string unless we write maximum lenght of the answer
+      if (input_answer.length() != output_text.length()) {
+        if (event.type == sf::Event::KeyPressed)
+        {
+          input_answer += event.key.code;
+        }
       }
     }
+
+    //now we can compare string and affect the damage
+    //TODO(igorkkkk) influence on player's life balancee
+    health_damage = differernce_lenght(input_answer, output_text);
 
 
     window.clear();
@@ -39,4 +49,21 @@ int main()
   }
 
   return 0;
+}
+
+
+
+auto differernce_lenght(std::string a, std::string b)->int {
+  auto itba = a.begin();
+  auto itbb = b.begin();
+  auto itea = a.end();
+  auto iteb = b.end();
+  int num = 0;
+  for (; itba != itea;itba++,itbb++) {
+    //check for the register?
+    if (*itba == *itbb) {
+      num++;
+    }
+  }
+  return a.length() - num;
 }

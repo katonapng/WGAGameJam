469eee7d573d1798c297d8c1265ba0c46d021b3f
Katona <annainterior14@gmail.com>
2021-12-11 21:24:31 +0300

    add menu implementation

--- FireSln/_Fire/DrawMenu.cpp
+++ FireSln/_Fire/DrawMenu.cpp
@@ -1,3 +1,97 @@
 #include "DrawMenu.h"
 
+using namespace std;
 
+DrawMenu::DrawMenu(sf::RenderWindow& window) {\
+
+	if (!gameName.loadFromFile("../MenuImages/rapid_fire_bullets2.png")) {
+		//handle error
+	}
+
+	if (!playButton.loadFromFile("../MenuImages/button_start_hover.png")) {
+		//handle error
+	}
+
+	if (!exitButton.loadFromFile("../MenuImages/button_exit_hover.png")) {
+		//handle error
+	}
+
+
+	if (!background.loadFromFile("../MenuImages/background.png")) {
+		//handle error
+	}
+
+	float width = window.getSize().x;
+	float height = window.getSize().y;
+
+	//needs modification, doesn't scale with window
+	playButtonSprite.setScale(0.7, 0.7); 
+	exitButtonSprite.setScale(0.5, 0.5);
+
+	sf::Vector2u gameNameSize = gameName.getSize();
+	sf::Vector2u playButtonSize = playButton.getSize();
+	sf::Vector2u exitButtonSize = exitButton.getSize();
+	sf::Vector2u backgroundSize = background.getSize();
+
+	float scaleX = (float)width / backgroundSize.x;
+	float scaleY = (float)height / backgroundSize.y;
+
+	backgroundSprite.setPosition(0,0);
+	backgroundSprite.setTexture(background);
+	backgroundSprite.setScale(scaleX, scaleY);
+
+	gameNameSprite.setPosition(width / 2 - gameNameSize.x / 2, height / 6);
+	gameNameSprite.setTexture(gameName);
+
+	playButtonSprite.setPosition(width / 2 - (playButtonSize.x *playButtonSprite.getScale().x) / 2, 1.8*height / 4);
+	playButtonSprite.setTexture(playButton);
+
+	exitButtonSprite.setPosition( width / 2 - (exitButtonSize.x * exitButtonSprite.getScale().x) / 2, 2.7*height /4);
+	exitButtonSprite.setTexture(exitButton);
+
+
+
+	//vector <string> texts = { "play", "exit" };
+	////vector <sf::Text::setFillColor()> colors = { sf::Color::Red, sf::Color::White, sf::Color::White };
+	//for (int i = 0; i < MAX_ITEM_NUMBER; ++i) {
+	//	text[i].setFont(font);
+	//	text[i].setString(texts[i]);
+	//	text[i].setPosition(sf::Vector2f(width / 2, height / (MAX_ITEM_NUMBER + 1) * (i + 1)));
+	//}
+}
+
+DrawMenu::~DrawMenu() {
+
+}
+
+
+void DrawMenu::Draw(sf::RenderWindow& window) {
+	/*for (int i = 0; i < MAX_ITEM_NUMBER; ++i) {
+		window.draw(text[i]);
+	}*/
+
+	window.draw(backgroundSprite);
+	window.draw(gameNameSprite);
+	window.draw(playButtonSprite);
+	window.draw(exitButtonSprite);
+}
+
+vector<int> DrawMenu::GetPosition(sf::Event &evnt) {
+	return  { evnt.mouseButton.x, evnt.mouseButton.y };
+}
+
+sf::FloatRect DrawMenu::playButtonGetRect() {
+	return playButtonSprite.getGlobalBounds();
+}
+
+sf::Vector2f DrawMenu::playButtonGetCenter() {
+	return playButtonSprite.getPosition();
+}
+
+sf::FloatRect DrawMenu::exitButtonGetRect() {
+	return exitButtonSprite.getGlobalBounds();
+}
+
+sf::Vector2f DrawMenu::exitButtonGetCenter() {
+	return exitButtonSprite.getPosition();
+}
--- FireSln/_Fire/main.cpp
+++ FireSln/_Fire/main.cpp
@@ -4,6 +4,9 @@
 #include <SFML/Window/Event.hpp>
 #include <string.h>
 #include <algorithm>
+#include "DrawMenu.h"
+
+using namespace std;
 
 
 auto differernce_lenght(std::string a, std::string b)->int;
@@ -11,24 +14,63 @@ auto differernce_lenght(std::string a, std::string b)->int;
 //using namespace std;
 int main()
 {
-  //I suppose to use all data about the word here to chek all collisions
+  //I want to use all data about the word here to check all collisions
   std::string game_location("0001000002"); //1 is player position
                                           //2 is object position
   std::string input_answer;
   std::string output_text;
-  float health_damage 0;
+  float health_damage = 0;
   //TODO(igorkkkk) add text downloading to output_text variable
 
-  sf::RenderWindow window(sf::VideoMode(640, 480), "Game"); //TODO(igorkkkk) change game name
+  sf::RenderWindow window(sf::VideoMode(2560, 1600), "Game", sf::Style::Fullscreen); //TODO(igorkkkk) change game name
+  //sf::Style::Fullscreen is used to avoid resizing problems
+
+  DrawMenu DrawMenu(window);
+
+  bool menuOpened = true;
 
   while (window.isOpen()) //main program
   {
     sf::Event event;
+
     while (window.pollEvent(event))
     {
-      if (event.type == sf::Event::Closed)
-        window.close();
+        if (event.type == sf::Event::Closed)
+            window.close();
+        switch (event.type)
+        {
+          case (sf::Event::MouseButtonPressed):
+              switch (event.mouseButton.button) {
+              case sf::Mouse::Left:
+                  vector<int> mousePosition = DrawMenu.GetPosition(event);
+                  sf::FloatRect playButtonRect = DrawMenu.playButtonGetRect();
+                  sf::Vector2f playButtonCenter = DrawMenu.playButtonGetCenter();
+                  sf::FloatRect exitButtonRect = DrawMenu.exitButtonGetRect();
+                  sf::Vector2f exitButtonCenter = DrawMenu.exitButtonGetCenter();
 
+                  if (mousePosition[0] <= (playButtonCenter.x + playButtonRect.width) 
+                      && mousePosition[0] >= (playButtonCenter.x)
+                      && mousePosition[1] <= (playButtonCenter.y + playButtonRect.height)
+                      && mousePosition[1] >= (playButtonCenter.y)) {
+                      //start the game
+                      menuOpened = false;
+                  }
+                  else if (mousePosition[0] <= (exitButtonCenter.x + exitButtonRect.width)
+                      && mousePosition[0] >= (exitButtonCenter.x)
+                      && mousePosition[1] <= (exitButtonCenter.y + exitButtonRect.height)
+                      && mousePosition[1] >= (exitButtonCenter.y)) {
+                      window.close();
+                  }
+                  break;
+              }
+          case (sf::Event::KeyReleased):
+              switch (event.key.code){
+              case sf::Keyboard::Escape:
+                  menuOpened = true;
+                  //change start sprite to continue 
+                  break;
+              }
+        }
       //input answer string unless we write maximum lenght of the answer
       if (input_answer.length() != output_text.length()) {
         if (event.type == sf::Event::KeyPressed)
@@ -40,10 +82,11 @@ int main()
 
     //now we can compare string and affect the damage
     //TODO(igorkkkk) influence on player's life balancee
-    health_damage = differernce_lenght(input_answer, output_text);
+    //health_damage = differernce_lenght(input_answer, output_text);
 
 
     window.clear();
+    if (menuOpened) DrawMenu.Draw(window);
    //TODO: draw all object before showing the main window
     window.display();
   }
@@ -53,7 +96,7 @@ int main()
 
 
 
-auto differernce_lenght(std::string a, std::string b)->int {
+auto difference_lenght(std::string a, std::string b)->int {
   auto itba = a.begin();
   auto itbb = b.begin();
   auto itea = a.end();
